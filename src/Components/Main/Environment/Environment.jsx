b5cb319008532db3e917a1d29b989d55959f0ad2
Voldemar <voldemar.turovskiy@gmail.com>
2021-01-27 16:24:30 +0400

    add_cards

--- src/Components/Main/Environment/Environment.jsx
+++ src/Components/Main/Environment/Environment.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import "antd/dist/antd.css";
 import "./Environment.css";
+import FeaturesPNG from "../../../img/features.png";
 import { Card, Button } from "react-bootstrap";
 
 function Environment() {
   return (
     <div className="Environment">
-        <div className="Environment__courses">
-            <div className="Environment__courses-mask">
-                <h4>- Individual approach</h4>
-                <h4>- Expert Teachers</h4>
-                <h4>- Online Education</h4>
-            </div>
+      <div className="Environment__courses">
+        <div className="Environment__courses-mask">
+          <h4>- Individual approach</h4>
+          <h4>- Expert Teachers</h4>
+          <h4>- Online Education</h4>
         </div>
+      </div>
+      <div className="Environment__block row justify-content-end">
+        <div className="row justify-content-end">
+          <div className="col-lg-8">
+            <h2 className="Environment-title">
+              Provide{" "}
+              <span>
+                best <br /> Educational
+              </span>{" "}
+              Environment
+            </h2>
+          </div>
+        </div>
+        <div className="row justify-content-end">
+          <div className="col-lg-11">
+            <div className="Environment-image">
+              <img src={FeaturesPNG} alt="features" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,6 +3,7 @@ import Slider from "./Slider/Slider";
 import Courses from "./Courses/Courses";
 import Environment from "./Environment/Environment";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Main.css"
 import { Link } from "react-router-dom";
 
 function Main() {
