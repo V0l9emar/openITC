bdf50f5573024a8855aeb87337755f254c8aca9d
Voldemar <voldemar.turovskiy@gmail.com>
2021-02-08 16:37:31 +0400

    add_cards

--- src/Components/Main/Courses/Courses.jsx
+++ src/Components/Main/Courses/Courses.jsx
@@ -3,90 +3,80 @@ import "antd/dist/antd.css";
 import "./Courses.css";
 import { Card, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGraduationCap, faChalkboard, faBookOpen, faUserGraduate } from "@fortawesome/free-solid-svg-icons";
-import Knowledge from "../../../img/knowledge.svg"
+import {
+  faGraduationCap,
+  faChalkboard,
+  faBookOpen,
+  faUserGraduate,
+} from "@fortawesome/free-solid-svg-icons";
+import Knowledge from "../../../img/knowledge.svg";
 
 function Courses() {
   return (
-    <div className="Courses">
-      {/* <Card className="Courses__card">
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-      </Card>
-      <Card className="Courses__card">
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-      </Card>
-      <Card className="Courses__card">
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Card Subtitle
-          </Card.Subtitle>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-      </Card> */}
-      <Card className="Courses__card">
-        {/* <Card.Img variant="top" src={Knowledge} /> */}
-        <FontAwesomeIcon className="Courses__card-icon" icon={faBookOpen} size="10x" color="#ef4323" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          {/* <Button variant="primary">Go somewhere</Button> */}
-        </Card.Body>
-      </Card>
-      <Card className="Courses__card">
-        {/* <Card.Img variant="top" src={Knowledge} /> */}
-        <FontAwesomeIcon className="Courses__card-icon" icon={faChalkboard} size="10x" color="#ef4323" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          {/* <Button variant="primary">Go somewhere</Button> */}
-        </Card.Body>
-      </Card>
-      <Card className="Courses__card">
-        {/* <Card.Img variant="top" src={Knowledge} /> */}
-        <FontAwesomeIcon className="Courses__card-icon" icon={faUserGraduate} size="10x" color="#ef4323" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          {/* <Button variant="primary">Go somewhere</Button> */}
-        </Card.Body>
-      </Card>
+    <div>
+      <div className="row">
+        <div className="col-lg-8">
+          <div className="section-title mt-40">
+            <h2 className="title">
+              Here is our <br /> Top Courses
+            </h2>
+            <p>
+              Even slightly believable. If you are going use a passage of Lorem
+              Ipsum need
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="Courses">
+        <Card className="Courses__card">
+          <FontAwesomeIcon
+            className="Courses__card-icon"
+            icon={faBookOpen}
+            size="10x"
+            color="#ef4323"
+          />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            {/* <Button variant="primary">Go somewhere</Button> */}
+          </Card.Body>
+        </Card>
+        <Card className="Courses__card">
+          <FontAwesomeIcon
+            className="Courses__card-icon"
+            icon={faChalkboard}
+            size="10x"
+            color="#ef4323"
+          />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            {/* <Button variant="primary">Go somewhere</Button> */}
+          </Card.Body>
+        </Card>
+        <Card className="Courses__card">
+          <FontAwesomeIcon
+            className="Courses__card-icon"
+            icon={faUserGraduate}
+            size="10x"
+            color="#ef4323"
+          />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            {/* <Button variant="primary">Go somewhere</Button> */}
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
